e697a969fbedecc0958bfb33f36f8eef3f62010d
Andrey Khalepov <andrey.khalepov@latoken.com>
2022-03-20 19:17:28 +1000

    env wallet

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::env;
 use tonic::{transport::Server, Request, Response, Status};
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::signature::Keypair;
@@ -9,14 +10,25 @@ use metaboss_service::decode::{decode_metadata_grpc};
 use metaboss_service::mint::mint_one_grpc;
 
 const URL: &str = "https://api.devnet.solana.com";
-const WALLET: [u8; 64] = [180,8,195,203,95,124,6,242,200,136,99,68,133,80,118,231,25,161,0,196,253,100,222,109,13,224,40,185,114,220,68,157,199,58,41,43,81,122,148,14,123,43,154,187,107,242,7,61,178,45,66,25,76,15,234,145,165,248,25,180,30,239,69,1];
 
 pub mod metaboss {
     tonic::include_proto!("metaboss");
 }
 
 #[derive(Debug, Default)]
-pub struct MyMetaboss {}
+pub struct MyMetaboss {
+    pub wallet: String,
+}
+
+impl MyMetaboss {
+    pub fn new() -> MyMetaboss {
+        let wallet = env::var("WALLET").expect("please set env variable WALLET");
+        MyMetaboss{
+            wallet
+        }
+    }
+}
+
 
 #[tonic::async_trait]
 impl Metaboss for MyMetaboss {
@@ -49,7 +61,7 @@ impl Metaboss for MyMetaboss {
         let request_message = request.into_inner();
         let mint = String::from(request_message.mint);
         let account = String::from(request_message.account);
-        let keypair = Keypair::from_bytes(&WALLET).unwrap();
+        let keypair= Keypair::from_base58_string(&self.wallet);
         match  burn_one_grpc(&client, keypair, mint, account) {
             Ok(signature) => {
                 let response = metaboss::BurnResponse {
@@ -69,7 +81,7 @@ impl Metaboss for MyMetaboss {
     ) -> Result<Response<MintResponse>, Status> {
         println!("Received request from: {:?}", request);
         let client = RpcClient::new(URL.to_string());
-        let keypair = Keypair::from_bytes(&WALLET).unwrap();
+        let keypair= Keypair::from_base58_string(&self.wallet);
         let request_message = request.into_inner();
 
         let receiver = String::from(request_message.receiver);
@@ -108,7 +120,7 @@ impl Metaboss for MyMetaboss {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "0.0.0.0:50051".parse()?;
-    let metaboss = MyMetaboss::default();
+    let metaboss = MyMetaboss::new();
 
     println!("Starting gRPC Server on port 50051...");
     Server::builder()
